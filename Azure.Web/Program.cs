cba76112d33c4a100adf7d126dabb5ef051d3bc6
santosh singh <santosh_singh2@dell.com>
2022-02-20 13:34:17 +0800

    Initial commit

---
+++ Azure.Web/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/error",()=>{
+    throw new Exception("oops");
+});
+
+app.Run();
